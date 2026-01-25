de7dfe2880229e6137be4dcaf137120c5d08096a
Chris Kaelin (EagleUK) <chriskaelin67@gmail.com>
2026-01-25 14:15:43 -0500

    completed store/retrieve JSON files lesson

---
+++ files-json/Services/BankAccountDTO.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+namespace Files_JSON;
+
+public class BankAccountDTO
+{
+    public int AccountNumber { get; set; }
+    public string CustomerId { get; set; } = "";
+    public double Balance { get; set; }
+    public string AccountType { get; set; } = "";
+    public double InterestRate { get; set; }
+
+    public static BankAccountDTO MapToDTO(BankAccount bankAccount)
+    {
+        return new BankAccountDTO
+        {
+            AccountNumber = bankAccount.AccountNumber,
+            CustomerId = bankAccount.CustomerId,
+            Balance = bankAccount.Balance,
+            AccountType = bankAccount.AccountType,
+            InterestRate = bankAccount.InterestRate
+        };
+    }
+}
